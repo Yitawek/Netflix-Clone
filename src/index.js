23d956faa95c5c7dda98982eda76e194541b7158
yitawek <yitawekmekuria@gmail.com>
2025-12-09 13:53:19 -0800

    add basename if shows only white screen

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom"; // <-- add this
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/netflix-clone">
+      {" "}
+      {/* <-- add basename if shows only white screen */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
