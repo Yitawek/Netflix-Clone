b2e74bd22a2484c88d57584dc1d381ea1b31bafa
yitawek <yitawekmekuria@gmail.com>
2025-12-09 14:06:47 -0800

    last try

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom"; // <-- add this
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Netflix-Clone">
+    <BrowserRouter basename="/netflix-clone">
       {/* <-- add basename if shows only white screen */}
       <App />
     </BrowserRouter>
